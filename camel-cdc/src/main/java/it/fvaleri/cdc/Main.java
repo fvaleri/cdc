54bb1cde077ae0449f086e285f75439847991f4f
Federico Valeri <fedevaleri@gmail.com>
2023-07-04 20:32:18 +0200

    Update plugins
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>

--- camel-cdc/src/main/java/it/fvaleri/cdc/Main.java
+++ camel-cdc/src/main/java/it/fvaleri/cdc/Main.java
@@ -1,10 +1,8 @@
 package it.fvaleri.cdc;
 
-import org.apache.camel.main.Main;
-
-public class Application {
+public class Main {
     public static void main(final String[] args) throws Exception {
-        final Main main = new Main();
+        final org.apache.camel.main.Main main = new org.apache.camel.main.Main();
         main.configure().addRoutesBuilder(new Routes());
         main.run(args);
     }
